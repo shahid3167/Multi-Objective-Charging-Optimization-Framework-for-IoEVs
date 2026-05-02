f6ec517a6517d4eb9435963f1779d51984fb8baa
Shahid Hussain <80464005+shahid3167@users.noreply.github.com>
2026-05-02 10:09:48 +0100

    Update main.py

--- SRC/MOCF_IoEV/main.py
+++ SRC/MOCF_IoEV/main.py
@@ -1,1 +1,10 @@
+def main():
+    print("MOCF-IoEVs Framework Running...\n")
 
+    print("1. Running Individual Charging Scenario")
+    print("2. Running Aggregated Charging Scenario")
+    print("3. Running Communication Model")
+
+
+if __name__ == "__main__":
+    main()
